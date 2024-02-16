b7e8a41cd04d508873f696d26602c0ec6f984bd9
feranzie <oladaviesferanmi@gmail.com>
2024-02-16 04:51:59 -0800

    b

--- week_3_data_warehouse/big_query.sql
+++ week_3_data_warehouse/big_query.sql
@@ -60,3 +60,65 @@ FROM taxi-rides-ny.nytaxi.yellow_tripdata_partitoned_clustered
 WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
   AND VendorID=1;
 
+
+--my solutione
+
+-- Creating external table referring to gcs path
+CREATE OR REPLACE EXTERNAL TABLE `de-zoomcamp-413508.dezoomcamp.external_yellow_tripdata`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://feranmi-prefect/yellow_tripdata_2020-*.csv', 'gs://feranmi-prefect/yellow_tripdata_2021-*.csv']
+);
+
+
+SELECT * FROM de-zoomcamp-413508.dezoomcamp.external_yellow_tripdata LIMIT 100;
+
+
+-- Create a non partitioned table from external table
+CREATE OR REPLACE TABLE de-zoomcamp-413508.dezoomcamp.yellow_tripdata_non_partitoned AS
+SELECT * FROM de-zoomcamp-413508.dezoomcamp.external_yellow_tripdata;
+
+-- Create a partitioned table from external table
+CREATE OR REPLACE TABLE de-zoomcamp-413508.dezoomcamp.yellow_tripdata_partitoned
+PARTITION BY
+  DATE(tpep_pickup_datetime) AS
+SELECT * FROM de-zoomcamp-413508.dezoomcamp.external_yellow_tripdata;
+
+-- Impact of partition
+-- Scanning 1.6GB of data
+SELECT DISTINCT(VendorID)
+FROM de-zoomcamp-413508.dezoomcamp.yellow_tripdata_non_partitoned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
+
+-- Scanning ~106 MB of DATA
+SELECT DISTINCT(VendorID)
+FROM de-zoomcamp-413508.dezoomcamp.yellow_tripdata_partitoned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
+
+-- Let's look into the partitons
+SELECT table_name, partition_id, total_rows
+FROM `dezoomcamp.INFORMATION_SCHEMA.PARTITIONS`
+WHERE table_name = 'yellow_tripdata_partitoned'
+ORDER BY total_rows DESC;
+
+-- Creating a partition and cluster table
+CREATE OR REPLACE TABLE de-zoomcamp-413508.dezoomcamp.yellow_tripdata_partitoned_clustered
+PARTITION BY DATE(tpep_pickup_datetime)
+CLUSTER BY VendorID AS
+SELECT * FROM de-zoomcamp-413508.dezoomcamp.external_yellow_tripdata;
+
+-- Query scans 1.1 GB
+SELECT count(*) as trips
+FROM de-zoomcamp-413508.dezoomcamp.yellow_tripdata_partitoned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
+  AND VendorID=1;
+
+-- Query scans 864.5 MB
+SELECT count(*) as trips
+FROM de-zoomcamp-413508.dezoomcamp.yellow_tripdata_partitoned_clustered
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
+  AND VendorID=1;
+
+
+
+
